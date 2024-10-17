0bdab605afb4d5b390e8694b4af663e5880c367d
mendeldebrabandere <mendel.debrabandere@hotmail.com>
2024-10-18 01:03:55 +0200

    Graphics pipeline

--- GridEngine/Renderer/GEPipeline.h
+++ GridEngine/Renderer/GEPipeline.h
@@ -12,13 +12,24 @@
 namespace GE {
 
 struct PipelineConfigInfo {
-
+	VkViewport viewport;
+	VkRect2D scissor;
+	VkPipelineViewportStateCreateInfo viewportInfo;
+	VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+	VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+	VkPipelineMultisampleStateCreateInfo multisampleInfo;
+	VkPipelineColorBlendAttachmentState colorBlendAttachment;
+	VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+	VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+	VkPipelineLayout pipelineLayout = nullptr;
+	VkRenderPass renderPass = nullptr;
+	uint32_t subpass = 0;
 };
 
 class GEPipeline {
 public:
 	GEPipeline(GEDevice &device, const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo& configInfo);
-	~GEPipeline(){};
+	~GEPipeline();
 
 	GEPipeline(const GEPipeline&) = delete;
 	GEPipeline& operator=(const GEPipeline&) = delete;
